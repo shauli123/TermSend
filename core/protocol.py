42ec01fab803ad62bfeaf49bf04e511a193d5f49
Shauli Sultan <sultansholi35@gmail.com>
2026-05-06 13:39:40 +0300

    addeed the protocol related classes

--- core/protocol.py
+++ core/protocol.py
@@ -1,14 +1,49 @@
 from cryptography.fernet import Fernet
 import json
+from enum import StrEnum, IntEnum, Enum, auto
+import exceptions
+
+class Side(Enum):
+    SERVER = auto()
+    CLIENT = auto()
+
+class ServerCommands(StrEnum):
+    REGISTER = "REGISTER"
+    LOGIN = "LOGIN"
+    SEND_MSG = "SEND_MSG"
+    RECIEVE_MSGS = "RECIEVE_MSGS"
+
+class ServerStatus(IntEnum):
+    # Status OK
+    OK = 200
+    SENDED = 201
+    # User errors
+    TOKEN_ERROR = 401
+    REGISTER_ERROR = 400
+    INVALID_USER = 404
+    LOGIN_ERROR = 405
+    # Genric Errors
+    UNSUPPORTED_COMMAND = 100
+    SERVER_ERROR = 505
 
 class Message():
-    def __init__(self, encrypted_msg: bytes, key: str):
+    def __init__(self, encrypted_msg: bytes, key: str, sender: Side):
         f = Fernet(key.encode())
         self.msg = f.decrypt(encrypted_msg).decode()
 
         msg_parts = self.msg.split('|', 3)
 
-        self.command = msg_parts[0]
+        if sender == Side.CLIENT:
+            if msg_parts[0] in ServerCommands:
+                self.command = ServerCommands(msg_parts[0])
+            else:
+                raise exceptions.UnsupportedCommand("The command is unsupported!")
+        else:
+            if int(msg_parts[0]) in ServerStatus:
+                self.status = ServerStatus(int(msg_parts[0]))
+            else:
+                raise exceptions.UnsupportedStatusCode("The status code is unsupported!")    
+
         if msg_parts[1] == "NONE":
             self.jwt = None
         else:
--- exceptions.py
+++ exceptions.py
@@ -19,3 +19,12 @@ class TokenError(UserError):
     def __init__(self, message):
         super().__init__(message)
 
+
+# Enum Errors
+class UnsupportedCommand(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class UnsupportedStatusCode(Exception):
+    def __init__(self, message):
+        super().__init__(message)
