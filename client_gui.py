a09f2edd46b71767c86cdbb9d8d3b68d36b524aa
Shauli Sultan <sultansholi35@gmail.com>
2026-07-13 12:56:18 +0300

    added login screen

--- client_gui.py
+++ client_gui.py
@@ -4,6 +4,8 @@ import client_util
 import ipaddress
 import socket
 
+JWT = None
+
 class ConnectScreen(ctk.CTkFrame):
     def __init__(self, master, on_connect, **kwargs):
         super().__init__(master, **kwargs)
@@ -71,8 +73,58 @@ class ConnectScreen(ctk.CTkFrame):
             self.status_label.configure(text="Cannot Connect!", text_color='red')
             return
 
-
+class LoginScreen(ctk.CTkFrame):
+    def __init__(self, master, on_login, **kwargs):
+        super().__init__(master, **kwargs)
+        self.on_login = on_login
         
+        self.center_frame = ctk.CTkFrame(master=self, fg_color="transparent")
+        self.center_frame.pack(expand=True, anchor="center")
+        
+        # Username Entry
+        self.username_label = ctk.CTkLabel(master=self.center_frame,text='Username:' )
+        self.username_entry = ctk.CTkEntry(master=self.center_frame, placeholder_text="username", corner_radius=8)
+        
+        self.username_label.grid(row = 0, column = 0, padx = 10, pady = 5)
+        self.username_entry.grid(row = 0, column = 1, padx = 10, pady = 5)
+        
+        # Password Entry
+        self.password_label = ctk.CTkLabel(master=self.center_frame,text='Password:' )
+        self.password_entry = ctk.CTkEntry(master=self.center_frame, placeholder_text="password", corner_radius=8, show='*')
+        
+        self.password_label.grid(row = 1, column = 0, padx = 10, pady = 5)
+        self.password_entry.grid(row = 1, column = 1, padx = 10, pady = 5)
+        
+        # Buttons
+        self.login_btn = ctk.CTkButton(master=self.center_frame, text='Login', command=lambda: self.on_submit('login'))
+        self.signup_btn = ctk.CTkButton(master=self.center_frame, text='Sign Up',  command=lambda: self.on_submit('signup'))
+        self.signup_btn.grid(row = 2, column = 0, padx = 10, pady = 5)
+        self.login_btn.grid(row = 2, column = 1, padx = 10, pady = 5)
+        
+        # Status Label
+        self.status_label = ctk.CTkLabel(master=self.center_frame,text='')
+        self.status_label.grid(row = 3, column = 0, padx = 10, pady = 5)
+
+    def on_submit(self, type: str):
+        global JWT
+        username = self.username_entry.get()  
+        password = self.password_entry.get()  
+
+        if type == 'signup':
+            try:
+                client_network.register(username, password)
+            except Exception as e:
+                self.status_label.configure(text = f"Error: {e}", text_color='red') 
+                return
+        try:
+            JWT, client_util.private_key = client_network.login(username, password)
+        except Exception as e:
+            self.status_label.configure(text = f"Error: {e}", text_color='red') 
+        else:
+            self.status_label.configure(text = f"Logged in!", text_color='green')
+            self.on_login()
+
+                
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -84,7 +136,16 @@ class App(ctk.CTk):
         self.connect_screen = ConnectScreen(self, self.on_connect_to_server)
         self.connect_screen.grid(row=0, column=0, sticky="nsew")
 
+        # Login screen
+        self.login_screen = LoginScreen(self, self.on_login)
+        
+        
+        
     def on_connect_to_server(self):
+        self.connect_screen.grid_forget()
+        self.login_screen.grid(row=0, column=0, sticky="nsew")
+
+    def on_login(self):
         pass
 
 if __name__ == '__main__':
